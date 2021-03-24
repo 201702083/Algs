2472ed3a14c20d96bd38c82693f5578d2cc9ea4f
201702083 <201702083@o.cnu.ac.kr>
2021-03-24 10:28:46 +0900

    setting

--- BOJ_py/Basic/Loop/N_1.py
+++ BOJ_py/Basic/Loop/N_1.py
@@ -1,11 +1,3 @@
 N = int(input())
-c = [0 for i in range(0,N)]
 for i in range(0,N):
-    a,b = input().split()
-    a = int(a)
-    b = int(b)
-    c[i] = a+b
-for i in range(0,N):
-    print('Case #' ,end='')
-    print(i+1,end=': ')
-    print(c[i])
+    print(N-i)
